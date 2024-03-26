f54bc47285020b259f7c0f43d80830235426adc8
Yeongh <108389613+andyj0927@users.noreply.github.com>
2024-03-26 11:04:14 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int MAXN = 1e6+5;
+int vis[MAXN];
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int a,b,t;
+  cin >> t;
+  while(t--){
+    cin >> a >> b;
+    cout << a+b << endl;
+  }
+}  
