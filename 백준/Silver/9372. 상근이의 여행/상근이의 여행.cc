a8ba125f57e797e3d11713a9e66f0c7c1138b7bc
Yeongh <108389613+andyj0927@users.noreply.github.com>
2024-04-29 19:02:46 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int t;
+  cin >> t;
+  while(t--){
+    int n,m;
+    cin >> n >> m;
+    while(m--){
+      int a,b;
+      cin >> a >> b;
+    }
+    cout << n-1 << endl;
+  }
+}  
