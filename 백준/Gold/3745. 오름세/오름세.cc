f079862c98bd75d40e9e73ac4e4c1371e3e18025
Yeongh <108389613+andyj0927@users.noreply.github.com>
2024-05-13 20:48:09 +0900

    [Gold II] Title: 오름세, Time: 40 ms, Memory: 48912 KB -BaekjoonHub

---
+++ 백준/Gold/3745. 오름세/오름세.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const ll MAXN = 4e6;
+ll n,h;
+ll arr[MAXN];
+pll a[1000005];
+bool compare(const pll &x, const pll &y){
+  if(x.first==y.first) return x.second>y.second;
+  return x.first < y.first;
+}
+void update(ll i , ll val){
+  i+=h;
+  arr[i]=val;
+  while(i>1){
+    i/=2;
+    arr[i] = max(arr[i*2],arr[i*2+1]);
+  }
+}
+ll mx(ll l , ll r, ll nodenum , ll nodel, ll noder){
+  if(nodel>r || noder<l) return 0;
+  if(l <= nodel && noder <= r) return arr[nodenum];
+  ll mid = (nodel+noder)/2;
+  return max(mx(l,r,nodenum*2,nodel,mid),mx(l,r,nodenum*2+1,mid+1,noder));
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int t = 0;
+  while(cin >> n){
+    h = 1 << ((int)ceil(log2(n)));
+    for(int i=1;i<2*h;i++) arr[i] = 0;
+    for(int i=0;i<n;i++){
+      cin >> a[i].fi;
+      a[i].se = i;
+      }
+    sort(a,a+n,compare);
+    for(int i=0;i<n;i++){
+      ll cur = a[i].second;
+      ll ncur =mx(0,cur,1,0,h-1);
+      update(cur,ncur+1);
+      }
+    cout << arr[1] << endl;
+  }
+}  
