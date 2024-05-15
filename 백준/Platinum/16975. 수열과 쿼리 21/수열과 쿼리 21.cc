22d56999120a6bf973e1a966393ed1418aa5ac80
Yeongh <108389613+andyj0927@users.noreply.github.com>
2024-05-15 13:32:48 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 56 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const ll MAXN = 4e6, INF = 1e7;
+ll n,m,h;
+ll arr[MAXN], a[100005];
+void update(ll i, ll val){
+  i+=h;
+  arr[i]+=val;
+  while(i>1){
+    i/=2;
+    arr[i] = arr[i*2]+arr[i*2+1];
+  }
+}
+ll cal(ll l , ll r, ll nodenum ,ll nodel, ll noder){
+  if(nodel >r || noder < l) return 0;
+  if(l <= nodel && noder <= r) return arr[nodenum];
+  ll mid = (nodel + noder)/2;
+  return cal(l,r,nodenum*2,nodel,mid)+cal(l,r,nodenum*2+1,mid+1,noder);
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cin >> n;
+  h = 1 << ((int)ceil(log2(1000000)));
+  for(int i=1;i<=n;i++) cin >> a[i];
+  cin >> m;
+  while(m--){
+    int q;
+    cin >> q;
+    if(q==1){
+      ll i,j,k;
+      cin >> i >> j >> k;
+      update(i-1,k);
+      update(j,-1*k);
+    }
+    else{
+      ll x;
+      cin >> x;
+      cout << cal(0,x-1,1,0,h-1)+a[x] << endl;
+    }
+  }
+}
