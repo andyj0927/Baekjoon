6f7b74a4d8bfe0539299a846eadca2a6abee5a3c
Yeongh <108389613+andyj0927@users.noreply.github.com>
2024-08-10 05:13:32 +0900

    [Platinum IV] Title: 광고, Time: 8 ms, Memory: 7076 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+vector <int> failure(string &s){
+  vector <int> f(s.size(),0);
+  int j=0;
+  for(int i=1;i<s.size();i++){
+    while(j>0 && s[i]!=s[j]) j = f[j-1];
+    if(s[i]==s[j]) f[i] = ++j;
+  }
+  return f;
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int l;
+  cin >> l;
+  string p;
+  cin >> p;
+  vector <int> f = failure(p);
+  cout << p.size()-f[p.size()-1];
+}
