d9ab2c2db856129a95e39b3aaf46697c42215d25
Yeongh <108389613+andyj0927@users.noreply.github.com>
2024-08-22 07:48:03 +0900

    [Platinum V] Title: 주유소, Time: 820 ms, Memory: 124980 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.cc
+++ 백준/Platinum/13308. 주유소/주유소.cc
@@ -5,7 +5,7 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 const ll MAXN = 1e14;
-map <pll,ll> d;
+ll d[2501][2501];
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -22,25 +22,25 @@ int main()
         adj[u].push_back({w,v});
         adj[v].push_back({w,u});
     }
-    d[{1,a[1]}] = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2500;j++) d[i][j] = MAXN;
+    }
     pq.push({0,{1,a[1]}});
+    d[1][a[1]] = 0;
     while(!pq.empty()){
         auto [val,cur] = pq.top();
         pq.pop();
-        if(d.find(cur)==d.end()) continue;
-        if(d[cur]!=val) continue;
+        if(d[cur.first][cur.second]!=val) continue;
         for(auto next : adj[cur.first]){
-            if(d.find({next.second,min(cur.second,a[next.second])})==d.end() || d[{next.second,min(cur.second,a[next.second])}]> d[cur]+next.first*cur.second){
-                d[{next.second,min(cur.second,a[next.second])}] = d[cur]+next.first*cur.second;
-                pq.push({d[cur]+next.first*cur.second,{next.second,min(cur.second,a[next.second])}});
+            if( d[next.second][min(cur.second,a[next.second])]> d[cur.first][cur.second]+next.first*cur.second){
+                d[next.second][min(cur.second,a[next.second])] = d[cur.first][cur.second]+next.first*cur.second;
+                pq.push({d[cur.first][cur.second]+next.first*cur.second,{next.second,min(cur.second,a[next.second])}});
             }
         }
     }
     ll ans = MAXN;
-    for(auto i : d){
-        if(i.first.first==n){
-            ans = min(ans,i.second);
-        }
+    for(int i=1;i<=2500;i++){
+        ans = min(ans,d[n][i]);
     }
     cout << ans;
 }
