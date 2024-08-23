3e7b8c24811eda1c5e87793ef515304e1a33a045
Yeongh <108389613+andyj0927@users.noreply.github.com>
2024-08-23 11:44:54 +0900

    [Platinum V] Title: 막대기, Time: 92 ms, Memory: 11240 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/8984. 막대기/막대기.cc
+++ 백준/Platinum/8984. 막대기/막대기.cc
@@ -5,7 +5,6 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 const ll MAXN = 1e14;
-ll d[100001][2];
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -21,30 +20,22 @@ int main()
     for(int i=0;i<n;i++){
         lena[i] = llabs(a[i].first-a[i].second)+l;
     }
-    map <pll,vector <int>> mp;
-    for(int i =0;i<n;i++){
-        mp[{a[i].first,0}].push_back(i);
-        mp[{a[i].second,1}].push_back(i);
+    map <ll,ll> up, down;
+    for(int i=0;i<n;i++){
+        up[a[i].first] = 0;
+        down[a[i].second] = 0;
     }
     for(int i=0;i<n;i++){
-        d[i][0] = lena[i];
-        d[i][1] = lena[i];
-        for(auto j : mp[{a[i].first,0}]){
-            if(i==j) continue;
-            if(a[i].second<a[j].second) continue;
-            d[i][0] = max(d[i][0],d[j][1]+lena[i]);
-        }
-        for(auto j : mp[{a[i].second,1}]){
-            if(i==j) continue;
-            if(a[i].first<a[j].first) continue;
-            d[i][1] = max(d[i][1],d[j][0]+lena[i]);
-        }
+        ll beup = up[a[i].first], bedown = down[a[i].second];
+        down[a[i].second] = max(bedown,beup+lena[i]);
+        up[a[i].first] = max(beup,bedown+lena[i]);
     }
     ll ans = 0;
-    for(int i=0;i<n;i++){
-        for(int j : {0,1}){
-            ans = max(ans,d[i][j]);
-        }
+    for(auto i : up){
+        ans = max(ans,i.second);
+    }
+    for(auto i : down){
+        ans = max(ans,i.second);
     }
     cout << ans;
 }
